f9df897acc5e6dc24db46d246dd32cc39b262d64
krzysztofskul <krzysztofskul@protonmail.com>
2024-04-19 15:15:58 +0200

    + initial commit with springboot configuration

---
+++ readme.md
@@ -0,0 +1,3 @@
+#### CADMDB | CADmedica database springboot application | hospital planning and calculation
+
+Springboot version of hobby application created for own developing skills in Java, Spring, Hiberante, JavaScript.
---
+++ src/main/java/pl/krzysztofskul/cadmdb/CadmdbApplication.java
@@ -0,0 +1,14 @@
+package pl.krzysztofskul.cadmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadmdbApplication.class, args);
+		//System.out.println("*TEST* CadmdbApplication.class");
+	}
+
+}
